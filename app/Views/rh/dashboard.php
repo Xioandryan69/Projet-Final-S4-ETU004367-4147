16efaeff13cb23da483e6f149e7b9cbb9212518d
nyandomm4 <nyandomm6@gmail.com>
2026-07-20 09:55:28 +0300

    ..
    .

--- app/Views/rh/dashboard.php
+++
@@ -1,3 +0,0 @@
-<h2>Espace Ressources Humaines </h2>
-<p>Bienvenue dans votre tableau de bord RH.</p>
-<a href="/logout">Se déconnecter</a>
--- app/Views/user/dashboard.php
+++
@@ -1,3 +0,0 @@
-<h2>Espace Utilisateur </h2>
-<p>Bienvenue sur votre profil.</p>
-<a href="/logout">Se déconnecter</a>
