b6b7d001ee9931c670624898a5576c72becee692
nyandomm4 <nyandomm6@gmail.com>
2026-07-20 10:42:37 +0300

    .

--- app/Controllers/AuthUtilisateurController.php
+++ app/Controllers/AuthUtilisateurController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\UtilisateurModel;
+use App\Models\CompteModel;
 
 class AuthUtilisateurController extends BaseController
 {
@@ -14,6 +15,53 @@ class AuthUtilisateurController extends BaseController
     public function loginPost()
     {
         $data = $this->request->getPost();
+
         $numero = $data['numero'];
+        // $motDePasse = $data['motDePasse'];
+
+        $compteModel = new CompteModel();
+
+        $compte = $compteModel
+            ->avecDetails()
+            ->where('Compte.numero', $numero)
+            ->first();
+
+
+        if ($compte) {
+
+            // // Vérification mot de passe
+            // if ($motDePasse == $compte['motDePasse']) {
+
+            //     session()->set([
+            //         'compte_id' => $compte['id'],
+            //         'numero' => $compte['numero'],
+            //         'solde' => $compte['solde'],
+            //         'utilisateur_id' => $compte['utilisateur_id'],
+            //         'nom' => $compte['nom'],
+            //         'prenom' => $compte['prenom'],
+            //         'operateur' => $compte['operateur'],
+            //         'typeCompte' => $compte['typeCompte'],
+            //         'connecte' => true
+            //     ]);
+
+
+            //     return $this->response->setJSON([
+            //         'status' => 'success',
+            //         'message' => 'Connexion réussie'
+            //     ]);
+            // } else {
+
+            //     return $this->response->setJSON([
+            //         'status' => 'error',
+            //         'message' => 'Mot de passe incorrect'
+            //     ]);
+            // }
+        } else {
+
+            return $this->response->setJSON([
+                'status' => 'error',
+                'message' => 'Compte introuvable'
+            ]);
+        }
     }
 }
