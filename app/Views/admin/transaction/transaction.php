71c69229b8532c523b935127b66040208de330a7
Andrianandrainy Ny Avo <andriannyavo9@gmail.com>
2026-07-20 10:27:31 +0300

    ok

---
+++ app/Views/admin/transaction/transaction.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="">Liste 1</a></li>
+            <li><a href="">LISTE 2</a></li>
+            <li><a href="">LISTE 2</a></li>
+            <li><a href="">LISTE 2</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
--- database.sql
+++ database.sql
@@ -29,6 +29,11 @@ CREATE TABLE Utilisateur (
     dateCreation DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE StatusCompte(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle  TEXT NOT NULL UNIQUE
+);
+
 CREATE TABLE Compte (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     numero TEXT NOT NULL UNIQUE,
@@ -38,9 +43,11 @@ CREATE TABLE Compte (
     typeOperateur_id INTEGER NOT NULL,
     typeCompte_id INTEGER NOT NULL,
     dateCreation DATETIME DEFAULT CURRENT_TIMESTAMP,
+    idStatusCompte INTEGER,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateur (id),
     FOREIGN KEY (typeOperateur_id) REFERENCES TypeOperateur (id),
-    FOREIGN KEY (typeCompte_id) REFERENCES TypeCompte (id)
+    FOREIGN KEY (typeCompte_id) REFERENCES TypeCompte (id),
+    FOREIGN KEY (idStatusCompte) REFERENCES StatusCompte(id)
 );
 
 CREATE TABLE TypeTransaction (
@@ -73,6 +80,11 @@ CREATE TABLE PrefixeNumero (
     FOREIGN KEY (typeOperateur_id) REFERENCES TypeOperateur (id)
 );
 
+CREATE TABLE StatusTransaction(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle TEXT NOT NULL
+);
+
 CREATE TABLE TransactionMobile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     typeTransaction_id INTEGER NOT NULL,
@@ -83,7 +95,8 @@ CREATE TABLE TransactionMobile (
     compteSource_id INTEGER,
     compteDestination_id INTEGER,
     raison TEXT,
-    statut TEXT NOT NULL DEFAULT 'VALIDE',
+    statutTransaction INTEGER,
+    FOREIGN KEY (statutTransaction) REFERENCES StatusTransaction (id),
     FOREIGN KEY (typeTransaction_id) REFERENCES TypeTransaction (id),
     FOREIGN KEY (compteSource_id) REFERENCES Compte (id),
     FOREIGN KEY (compteDestination_id) REFERENCES Compte (id)
